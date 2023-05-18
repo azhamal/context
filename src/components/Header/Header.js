7ff7093ef9d7f1447766ebec00e96c1c26153f15
azhamal <aizhamaltolubaeva@gmail.com>
2023-05-18 10:12:40 +0600

    address change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header =()=> {
             <Link to={'/goods'}> Товары</Link>
             <Link to={'/about'}> О нас</Link>
             <Link to={'/contacts'}> Контакты</Link>
-            <Link to={'/address'}> Адрес</Link>
+            <Link to={'/address'}> Адресc</Link>
             {
             token?
             <button onClick={()=>logout}>Выйти из аккаунт</button>
